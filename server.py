9a3930eb261b04ad8c25f0fcebffe16831a1f5c5
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2024-01-26 23:02:59 -0600

    Updated server.py: completed level 3

--- server.py
+++ server.py
@@ -13,5 +13,9 @@ def level_1():
 def level_2(num):
     return render_template("home.html", color = "aqua", num=num)
 
+@app.route("/play/<int:num>/<string:color>")
+def level_3(num,color):
+    return render_template("home.html", num=num, color=color)
+
 if __name__ == "__main__":
     app.run(debug=True)
